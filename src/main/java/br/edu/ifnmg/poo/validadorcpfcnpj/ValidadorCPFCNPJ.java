14aedf0bc4e2f559c26578f90bd98f06b466fc71
AnaLuizanc <analuizanc13@gmail.com>
2024-07-03 14:12:26 -0300

    add getters e setters

--- src/main/java/br/edu/ifnmg/poo/validadorcpfcnpj/ValidadorCPFCNPJ.java
+++ src/main/java/br/edu/ifnmg/poo/validadorcpfcnpj/ValidadorCPFCNPJ.java
@@ -5,8 +5,26 @@ package br.edu.ifnmg.poo.validadorcpfcnpj;
  * @author analu
  */
 public class ValidadorCPFCNPJ {
+
     private long numero;
     private String numeroString;
-    
-    
+
+    //<editor-fold defaultstate="collapsed" desc="Getters e Setters">
+    public long getNumero() {
+        return numero;
+    }
+
+    public void setNumero(long numero) {
+        this.numero = numero;
+    }
+
+    public String getNumeroString() {
+        return numeroString;
+    }
+
+    public void setNumeroString(String numeroString) {
+        this.numeroString = numeroString;
+    }
+
+//</editor-fold>
 }
