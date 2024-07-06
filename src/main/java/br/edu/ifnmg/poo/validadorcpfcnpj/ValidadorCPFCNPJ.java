7872e67dabcd96086c883b1dc5c742560964b106
AnaLuizanc <analuizanc13@gmail.com>
2024-07-06 20:19:20 -0300

    exclusão atributos, getters e setters

--- src/main/java/br/edu/ifnmg/poo/validadorcpfcnpj/ValidadorCPFCNPJ.java
+++ src/main/java/br/edu/ifnmg/poo/validadorcpfcnpj/ValidadorCPFCNPJ.java
@@ -6,27 +6,8 @@ package br.edu.ifnmg.poo.validadorcpfcnpj;
  */
 public class ValidadorCPFCNPJ {
 
-    private static long numero;
-    private static String numeroString;
+    public static void isCpfValido(long numero) {
 
-    //<editor-fold defaultstate="collapsed" desc="Getters e Setters">
-    public static long getNumero() {
-        return numero;
     }
-
-    public static void setNumero(long numero) {
-        ValidadorCPFCNPJ.numero = numero;
-    }
-
-    public static String getNumeroString() {
-        return numeroString;
-    }
-
-    public static void setNumeroString(String numeroString) {
-        ValidadorCPFCNPJ.numeroString = numeroString;
-    }
-
-//</editor-fold>
-
 
 }
