5741e53eb9e405aecfab873ed73dd0643eea71ca
AnaLuizanc <analuizanc13@gmail.com>
2024-07-06 21:01:04 -0300

    add dos métodos para verificação de CPF dado como inteiro longo

--- src/main/java/br/edu/ifnmg/poo/validadorcpfcnpj/ValidadorCPFCNPJ.java
+++ src/main/java/br/edu/ifnmg/poo/validadorcpfcnpj/ValidadorCPFCNPJ.java
@@ -6,8 +6,32 @@ package br.edu.ifnmg.poo.validadorcpfcnpj;
  */
 public class ValidadorCPFCNPJ {
 
-    public static void isCpfValido(long numero) {
+    public static boolean isCpfValido(long cpf) throws CpfInvalidoException {
+        long digitoVerificador = cpf % 100;
+        int digitoVerificadorGerado = geradorDigitoVerificador(cpf / 100);
+        digitoVerificadorGerado = digitoVerificadorGerado * 10 + geradorDigitoVerificador(cpf / 10);
+        if (digitoVerificador == digitoVerificadorGerado) {
+            System.out.println(cpf + "\nVálido.");
+            return true;
+        } else {
+            System.out.println(cpf);
+            throw new CpfInvalidoException();
+        }
+    }
 
+    public static int geradorDigitoVerificador(long cpf) {
+        int somatorio = 0;
+        int indice = 9;
+        while (cpf > 0) {
+            somatorio += obterDigito(cpf, indice) * indice;
+            indice--;
+            cpf /= 10;
+        }
+        return somatorio % 11;
+    }
+
+    public static int obterDigito(long cpf, int indice) {
+        return (int) cpf % 10;
     }
 
 }
