9d30210b50aa4466cf3cad27615a6d858faf219d
AnaLuizanc <analuizanc13@gmail.com>
2024-07-07 13:42:18 -0300

    add método de verificação de CNPJ com string

--- src/main/java/br/edu/ifnmg/poo/validadorcpfcnpj/ValidadorCPFCNPJ.java
+++ src/main/java/br/edu/ifnmg/poo/validadorcpfcnpj/ValidadorCPFCNPJ.java
@@ -93,5 +93,21 @@ public class ValidadorCPFCNPJ {
         }
     }
 
+    public static boolean isCnpjInvalido(String cnpj) throws CnpjInvalidoException {
+        long cnpjLong = converterStringParaLong(removerSimbolosCnpj(cnpj));
+        long digitoVerificador = cnpjLong % 100;
+        int digitoVerificadorGerado = geradorDigitoVerificadorCnpj(cnpjLong / 100);
+        digitoVerificadorGerado = digitoVerificadorGerado * 10 + geradorDigitoVerificadorCnpj(cnpjLong / 10);
+        if (digitoVerificador == digitoVerificadorGerado) {
+            System.out.println(cnpj + "\nVálido.");
+            return true;
+        } else {
+            System.out.println(cnpj);
+            throw new CnpjInvalidoException();
+        }
+    }
 
+    public static String removerSimbolosCnpj(String cnpj) {
+        return cnpj.substring(0, 2) + cnpj.substring(3, 6) + cnpj.substring(7, 10) + cnpj.substring(11, 15) + cnpj.substring(cnpj.indexOf("-") + 1);
+    }
 }
