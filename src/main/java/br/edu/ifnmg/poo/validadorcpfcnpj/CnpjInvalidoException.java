759daf91dfee6529c7060e68038b607c8fa6fc03
AnaLuizanc <analuizanc13@gmail.com>
2024-07-07 12:22:39 -0300

    criação da exceção do CNPJ

---
+++ src/main/java/br/edu/ifnmg/poo/validadorcpfcnpj/CnpjInvalidoException.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package br.edu.ifnmg.poo.validadorcpfcnpj;
+
+/**
+ *
+ * @author analu
+ */
+public class CnpjInvalidoException extends Exception {
+
+    public CnpjInvalidoException() {
+        super("Inválido");
+    }
+
+}
